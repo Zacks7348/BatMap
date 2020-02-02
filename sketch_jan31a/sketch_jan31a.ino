1cd1aef37017ee83ec3cb33d2b951e613ff9e36b
Zackary Schreiner <39150651+Zacks7348@users.noreply.github.com>
2020-02-02 09:46:20 -0500

    Update sketch_jan31a.ino

--- sketch_jan31a/sketch_jan31a.ino
+++ sketch_jan31a/sketch_jan31a.ino
@@ -7,39 +7,24 @@ const int echoPin = 10;
 //Ultrasonic Variables
 float duration, distance;
 
-// Button constants won't change. They're used here to set pin numbers:
-const int buttonPin = 2;     // the number of the pushbutton pin
-const int ledPin =  13;      // the number of the LED pin
-
-// Button Variables
-int buttonState = 0;         // variable for reading the pushbutton status
-boolean ON = false;
-
-//Bluetooth Variables
-#include <SoftwareSerial.h>
-SoftwareSerial hc06(2,3);
-
 void setup() {
   Serial.begin(9600);
   // initialize the pushbutton pin as an input:
-  pinMode(buttonPin, INPUT);
 
   //Initialize the Sensor
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+}
 
-  //Initialize bluetooth
-  hc06.begin(9600);
+void ping(int n) {
+  for (int i = 0; i < n; i++) {
+    Serial.println("PING");
+  }
+   Serial.println(" ");
 }
 
 void batMap() {
-  //Slow down how often the distance is measured
-  if (hc06.read() != -1) {
-    if (digitalRead(buttonPin) == 1) {
-      ON = !ON;
-    }
-  }
-  if ((millis() - lastRead >= INTERVAL) and ON) {
+  if ((millis() - lastRead >= INTERVAL)) {
     digitalWrite(trigPin, LOW);
     delayMicroseconds(2);
     digitalWrite(trigPin, HIGH);
@@ -51,10 +36,20 @@ void batMap() {
     //Calculate distance using formula
     distance = (duration * .0343) / 2;
     //Filter out the distance
-    if (distance < 500) {
-      Serial.print("Distance: ");
-      Serial.println(distance);
-      hc06.print(distance);
+    if (distance > 400 and distance <= 500) {
+      ping(1);
+    }
+    else if (distance > 300 and distance <= 400) {
+      ping(2);
+    }
+    else if (distance > 200 and distance <= 300) {
+      ping(3);
+    }
+    else if (distance > 100 and distance <= 200) {
+      ping(4);
+    }
+    else if (distance > 0 and distance <= 100) {
+      ping(5);
     }
     delay(100);
     //Reset counter back to the current time
