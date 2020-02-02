74cec599bc763f287784d0fabc9436564fae4f9a
Zackary Schreiner <39150651+Zacks7348@users.noreply.github.com>
2020-02-02 11:34:06 -0500

    Update sketch_jan31a.ino

--- sketch_jan31a/sketch_jan31a.ino
+++ sketch_jan31a/sketch_jan31a.ino
@@ -16,15 +16,17 @@ void setup() {
   pinMode(echoPin, INPUT);
 }
 
-void ping(int n) {
+void ping(int n, float distance) {
   for (int i = 0; i < n; i++) {
-    Serial.println("PING");
+    Serial.print("BatMap: PING (");
+    Serial.print(distance);
+    Serial.println(" cm)");
   }
-   Serial.println(" ");
 }
 
 void batMap() {
   if ((millis() - lastRead >= INTERVAL)) {
+    Serial.println("BatMap: ");
     digitalWrite(trigPin, LOW);
     delayMicroseconds(2);
     digitalWrite(trigPin, HIGH);
@@ -37,19 +39,19 @@ void batMap() {
     distance = (duration * .0343) / 2;
     //Filter out the distance
     if (distance > 400 and distance <= 500) {
-      ping(1);
+      ping(1, distance);
     }
     else if (distance > 300 and distance <= 400) {
-      ping(2);
+      ping(2, distance);
     }
     else if (distance > 200 and distance <= 300) {
-      ping(3);
+      ping(3, distance);
     }
     else if (distance > 100 and distance <= 200) {
-      ping(4);
+      ping(4, distance);
     }
     else if (distance > 0 and distance <= 100) {
-      ping(5);
+      ping(5, distance);
     }
     delay(100);
     //Reset counter back to the current time
