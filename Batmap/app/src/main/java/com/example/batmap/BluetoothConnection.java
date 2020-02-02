f26aa031e5bf0c07f2dd529f40fdd42d06c9119d
lvismf <57381313+lvismf@users.noreply.github.com>
2020-02-01 19:24:36 -0500

    Add files via upload
    trial & error

---
+++ Batmap/app/src/main/java/com/example/batmap/BluetoothConnection.java
@@ -0,0 +1,6 @@
+package com.example.batmap;
+
+public class BluetoothConnection {
+
+
+}
--- Batmap/app/src/main/java/com/example/batmap/MainActivity.java
+++ Batmap/app/src/main/java/com/example/batmap/MainActivity.java
@@ -4,16 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import java.lang.*;
 
+import java.lang.*;
 import java.util.Set;
 
-import static android.provider.Settings.Global.DEVICE_NAME;
+
 
 public class MainActivity extends AppCompatActivity {
+
+BluetoothAdapter adapter;
+BluetoothAdapter btAdapter;
+BluetoothSocket socket;
+BluetoothDevice BLdevice;
+
+Button mainButton;
+
+boolean deviceFound = false;
+String DEVICE_NAME = "";
 
 
 
@@ -21,6 +32,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainButton = findViewById(R.id.onOFF);
+
+        findDevice();
+
+        mainButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+            }
+
+        });
+    }
+
+
+    public void findDevice() {
+        Set<BluetoothDevice> pairedDevices = adapter.getBondedDevices();
+        if (pairedDevices.size() > 0) {
+            for (BluetoothDevice device : pairedDevices) {
+                if (device.getName().equals(DEVICE_NAME)) {
+                   BLdevice = device;
+
+
+
+                    deviceFound = true;
+                    break;
+                }
+            }
+        }
     }
 
 }
